9fc2d945f4e9ea0a9fe54e3ed4eebe1c5c3ed3b3
Augusto <augusto-vial@hotmail.com>
2023-04-05 22:31:56 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function abrirLista() {
+    lista = document.getElementById('dropdown')
+
+    if (lista.style.display == "none"){
+        lista.style.display= '';
+    } else {
+        lista.style.display = "none"
+    }
+}
