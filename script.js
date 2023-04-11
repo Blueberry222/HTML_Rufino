32608b3cfc0cd8404cc322f0ad5db64f659064b6
Augusto <augusto-vial@hotmail.com>
2023-04-10 22:15:33 -0300

    Finalizado?

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 function abrirLista() {
-    lista = document.getElementById('dropdown')
+
+    lista = document.getElementById('minhaLista')
+    botao = document.getElementById("botao");
 
     if (lista.style.display == "none"){
-        lista.style.display= '';
+        lista.style.display= ''; // Abrir
     } else {
-        lista.style.display = "none"
+        lista.style.display = "none"; // Fechar
+        botao.blur();
     }
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.btnAbrir')) { // Se clicar em qualquer lugar que não é o botão
+        lista = document.getElementById('minhaLista');
+        lista.style.display = "none";
+        botao.blur();
+    } 
 }
